ecc77c336e13c65a1bd34eff0a2904b67a317991
NirajGohel <nirajgohel.ng@gmail.com>
2021-01-15 20:30:04 +0530

    login and signup added

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 //import routes
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
 
 //connect DB
-mongoose.connect(
-        'mongodb+srv://sdp_users:Sd@123456@cluster0.xcct7.mongodb.net/SDP?retryWrites=true&w=majority', 
-        {useUnifiedTopology: true,
-        useNewUrlParser: true }
-).then(
-    () => console.log('DB connected!!'))
-    .catch(err => {
-
-        console.log(Error,err.message);
-
-    });
+mongoose
+  .connect(
+    "mongodb+srv://sdp_users:Sd@123456@cluster0.xcct7.mongodb.net/SDP?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(() => console.log("DB connected!!"))
+  .catch((err) => {
+    console.log(Error, err.message);
+  });
 
 //middleware
+app.use(cors());
 app.use(express.json());
 
-
 //route middleware
-app.use('/api/scorer',authRoute);
+app.use("/api/scorer", authRoute);
 
-
-app.listen(3000, () => console.log('server Up and running'));
+app.listen(3000, () => console.log("server Up and running"));
