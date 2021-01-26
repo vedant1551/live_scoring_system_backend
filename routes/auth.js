c9fa738d0864f4d122343f3ea3ab4ad60b3378c4
utsavraychura11 <utsavraychura11@gmail.com>
2021-01-26 22:48:53 +0530

    26/01/2021

--- routes/auth.js
+++ routes/auth.js
@@ -4,82 +4,83 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
 router.get("/", (req, res) => {
-  res.send("hello");
+    res.send("hello");
 });
 
-router.post("/register", async (req, res) => {
-  const scorer = new Scorer(req.body);
+router.post("/register", async(req, res) => {
+    const scorer = new Scorer(req.body);
 
-  try {
-    const savedScorer = await scorer.save();
-    res.send(savedScorer);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const scorer = await Scorer.findOne({ email: req.body.email });
-  if (!scorer) return res.status(404).send("User does not exist!");
-
-  if (req.body.password != scorer.password)
-    return res.status(422).send("Password is not correct!");
-
-  //creat and assign jwt
-  const token = jwt.sign({ _id: scorer._id }, "unvunv");
-  res.header("auth-token", token).send(token);
-});
-
-router.post("/forgotpassword", async (req, res) => {
-  const scorer = await Scorer.findOne({ email: req.body.email });
-  if (!scorer) return res.status(404).send("User does not exist!");
-  
-  var digits = '0123456789'; 
-  let otp = ''; 
-  for (let i = 0; i < 6; i++ ) { 
-      otp += digits[Math.floor(Math.random() * 10)]; 
-  }
-  var message1 = "Your otp for Scoring system is " + otp
-
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'emailidbanavvupadse',
-      pass: 'emailidbanavvupadse'
+    try {
+        const savedScorer = await scorer.save();
+        res.send(savedScorer);
+    } catch (err) {
+        res.status(400).send(err);
     }
-  });
-
-  var mailOptions = {
-    from: 'vedantrajyaguru.333@gmail.com',
-    to: req.body.email,
-    subject: 'Forgot password,- Live Scoring system',
-    text: message1
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      res.send(error);
-    } else {
-      res.header("otp", otp).send();
-    }
-  });
-
-  res.header("otp", otp).send();
 });
 
-router.put("/changepassword", async (req, res) => {
-  const scorer = await Scorer.findOne({ email: req.body.email });
-  if (!scorer) return res.status(404).send("User does not exist!");
+router.post("/login", async(req, res) => {
+    const scorer = await Scorer.findOne({ email: req.body.email });
+    if (!scorer) return res.status(404).send("User does not exist!");
 
-  scorer.body.password = req.password;
-  res.send();
-  
-  try {
-    const savedScorer = await scorer.save();
-    res.send(savedScorer);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+    if (req.body.password != scorer.password)
+        return res.status(422).send("Password is not correct!");
+
+    //creat and assign jwt
+    const token = jwt.sign({ _id: scorer._id }, "unvunv");
+    res.header("auth-token", token).send(token);
+});
+
+
+router.post("/forgotpassword", async(req, res) => {
+    const scorer = await Scorer.findOne({ email: req.body.email });
+    if (!scorer) return res.status(404).send("User does not exist!");
+
+    var digits = '0123456789';
+    let otp = '';
+    for (let i = 0; i < 6; i++) {
+        otp += digits[Math.floor(Math.random() * 10)];
+    }
+    var message1 = "Your otp for Scoring system is " + otp
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'shreejitiffinservice2@gmail.com',
+            pass: '@Shreeji123'
+        }
+    });
+
+    var mailOptions = {
+        from: 'shreejitiffinservice2@gmail.com',
+        to: req.body.email,
+        subject: 'Forgot password,- Live Scoring system',
+        text: message1
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return res.send(error);
+        } else {
+            return res.send(otp);
+        }
+    });
+
+});
+
+
+router.put("/changepassword", async(req, res) => {
+    const scorer = await Scorer.findOne({ email: req.body.email });
+    if (!scorer) return res.status(404).send("User does not exist!");
+
+    scorer.password = req.body.password;
+    //res.header("otp", otp).send();
+
+    try {
+        const savedScorer = await scorer.save();
+        res.send(savedScorer);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 
 });
 
